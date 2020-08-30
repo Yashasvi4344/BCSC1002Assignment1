16c135a37e5db7f91e18853e5f9453777ca343f3
yashasvi singh <yashasvi.singh_cs19@gla.ac.in>
2020-08-30 19:14:02 +0530

    add Scanner class from java.util.Scanner package to FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,8 +6,16 @@
  * */
 package execution;
 
+import java.util.Scanner;
 public class FrontDesk {
     public static void main(String[] args) {
 
-    }
-}
+
+        public class FrontDesk {
+            private static final int ISSUE_A_NEW_BOOK = 3;
+            private static final int RETURN_A_PREVIOUSLY_ISSUED_BOOK = 5;
+            private static final int SHOW_ALL_ISSUED_BOOKS = 8;
+            private static final int EXIT = 0;
+
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
