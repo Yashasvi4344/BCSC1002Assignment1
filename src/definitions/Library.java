ac59a45e02edb330db0b18ca3511b78387d6d54a
yashasvi singh <yashasvi.singh_cs19@gla.ac.in>
2020-08-30 17:25:24 +0530

    add getter and setter to Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -22,3 +22,11 @@ public class Library {
             this.booksThatAreCurrentlyAvailable[libraryIndex] = new Book();
         }
     }
+
+    public Book[] getBooksThatAreCurrentlyAvailable() {
+        return booksThatAreCurrentlyAvailable;
+    }
+
+    public void setBooksThatAreCurrentlyAvailable(Book[] booksThatAreCurrentlyAvailable) {
+        this.booksThatAreCurrentlyAvailable = booksThatAreCurrentlyAvailable;
+    }
